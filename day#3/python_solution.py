dc410cc534b0ce27d0fc032177bd091de807ae28
Zulejman <zzullezz@gmail.com>
2024-12-04 22:50:06 +0100

    day 3: part 1 solution

---
+++ day#3/python_solution.py
@@ -0,0 +1,16 @@
+import re
+
+uncorr_line = []
+total_value = 0
+
+with open("input.txt", "r") as file_data:
+    for line in file_data:
+        uncorr_line = re.findall("mul\((\d{1,3}),(\d{1,3})\)", line)
+        for data_entry in uncorr_line:
+            multy = int(data_entry[0]) * int(data_entry[1])
+            total_value += multy
+
+
+file_data.close()
+print(total_value)
+
